e89d4f61a0537d6b1cd29d045959e8a514f9d651
John Kim <abcdefghijohnk@gmail.com>
2020-11-28 13:07:20 -0700

    fix spelling of astroid to asteroid

---
+++ asteroid.rb
@@ -0,0 +1,11 @@
+class Asteroid
+  def initialize(info)
+    @name = info[:name]
+    @diameter = info[:diameter]
+    @miss_distance = info[:miss_distance]
+  end
+
+  def self.get_diameter(asteroid)
+    
+  end
+end
