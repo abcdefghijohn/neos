b2fbb1e5027013302a1d91f28f6ec0ca255aeacb
John Kim <abcdefghijohnk@gmail.com>
2020-11-28 13:25:28 -0700

    update asteroid class with new diameter and miss distance

--- asteroid.rb
+++ asteroid.rb
@@ -1,11 +1,7 @@
 class Asteroid
   def initialize(info)
     @name = info[:name]
-    @diameter = info[:diameter]
-    @miss_distance = info[:miss_distance]
-  end
-
-  def self.get_diameter(asteroid)
-    
+    @diameter = info[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+    @miss_distance = info[:close_approach_data][0][:miss_distance][:miles].to_i
   end
 end
